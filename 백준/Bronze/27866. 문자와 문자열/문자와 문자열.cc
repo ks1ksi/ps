30d5dfe1c9fa250d912710466865d303954905fc
Seungil Kim <ksi990302@gmail.com>
2023-08-13 21:24:41 +0900

    [Bronze V] Title: 문자와 문자열, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Bronze/27866. 문자와 문자열/문자와 문자열.cc
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+using ll = long long;
+using ld = long double;
+
+int main() {
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+
+    string s;
+    int x;
+
+    cin >> s;
+    cin >> x;
+
+    cout << s[x - 1] << '\n';
+
+    return 0;
+}
