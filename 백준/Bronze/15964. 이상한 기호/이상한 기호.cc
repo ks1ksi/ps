c466db2761ba42e798b05bfbe56030f599c4b523
Seungil Kim <ksi990302@gmail.com>
2023-11-21 05:06:53 +0900

    [Bronze V] Title: 이상한 기호, Time: 0 ms, Memory: 2020 KB, Score: 100 point -BaekjoonHub

---
+++ 백준/Bronze/15964. 이상한 기호/이상한 기호.cc
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+using ll = long long;
+using ld = long double;
+
+int main() {
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+
+    ll a, b;
+    cin >> a >> b;
+
+    cout << (a + b) * (a - b) << '\n';
+
+    return 0;
+}
