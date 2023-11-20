02b0f0bf32113c0d731295a7f031e22a77c7d9f6
Seungil Kim <ksi990302@gmail.com>
2023-11-21 04:41:11 +0900

    [Bronze V] Title: 사파리월드, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/2420. 사파리월드/사파리월드.cc
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+using ll = long long;
+using ld = long double;
+
+int main() {
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+
+    ll n, m;
+    cin >> n >> m;
+
+    cout << abs(n - m) << '\n';
+
+    return 0;
+}
