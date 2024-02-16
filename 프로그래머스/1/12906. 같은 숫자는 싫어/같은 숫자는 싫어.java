c20e7f63ac60186b13909e9e298ddd88db2ce6a5
Seungil Kim <ksi990302@gmail.com>
2024-02-16 12:44:52 +0900

    [level 1] Title: 같은 숫자는 싫어, Time: 35.18 ms, Memory: 117 MB -BaekjoonHub

---
+++ 프로그래머스/1/12906. 같은 숫자는 싫어/같은 숫자는 싫어.java
@@ -0,0 +1,20 @@
+import java.util.*;
+import java.util.stream.*;
+
+public class Solution {
+    public int[] solution(int []arr) {
+        List<Integer> list = new ArrayList<>();
+        list.add(arr[0]);
+        
+        for (int i = 1; i < arr.length; i++) {
+            if (arr[i - 1] == arr[i]) {
+                continue;
+            }
+            list.add(arr[i]);
+        }
+        
+        int[] answer = list.stream().mapToInt(Integer::intValue).toArray();
+
+        return answer;
+    }
+}
